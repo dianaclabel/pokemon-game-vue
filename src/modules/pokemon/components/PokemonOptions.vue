8e38f7013b796f3c9deb834378222753d48b2e55
diana <dianaclabel08@gmail.com>
2025-02-04 22:23:28 -0500

    Se realiza un sistema de archivos y estructura del proyecto

---
+++ src/modules/pokemon/components/PokemonOptions.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="mt-5">
+    <ul>
+      <li :class="liClasses">Pokemon 1</li>
+      <li :class="liClasses">Pokemon 2</li>
+      <li :class="liClasses">Pokemon 3</li>
+      <li :class="liClasses">Pokemon 4</li>
+    </ul>
+  </section>
+</template>
+
+<script setup lang="ts">
+const liClasses =
+  'bg-white shadow-md rounded-lg p-3 m-2 cursor-pointer w-40 text-center transition-all hover:bg-gray-100';
+</script>
