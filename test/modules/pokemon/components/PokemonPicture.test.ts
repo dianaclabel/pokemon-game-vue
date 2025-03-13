5fbb08661da2007091de4de91b8259b1dc59238f
diana <dianaclabel08@gmail.com>
2025-03-12 23:09:33 -0500

    Add test for rendering image source in PokemonPicture component

--- test/modules/pokemon/components/PokemonPicture.test.ts
+++ test/modules/pokemon/components/PokemonPicture.test.ts
@@ -2,6 +2,8 @@ import PokemonPicture from '@/modules/pokemon/components/PokemonPicture.vue';
 import { mount } from '@vue/test-utils';
 
 describe('<Name of the group />', () => {
+  const imageSource = '';
+
   test('Should render the hidden image when showPokemon props is false', async () => {
     const wrapper = mount(PokemonPicture, {
       props: {
@@ -10,7 +12,12 @@ describe('<Name of the group />', () => {
       },
     });
 
-    expect(wrapper.find('img').classes());
+    console.log(wrapper.html());
+
+    const image = wrapper.find('img');
+
+    expect(wrapper.find('img')).toBeTruthy();
+    expect(image.attributes('src')).toBe(imageSource);
   });
 
   test('Should rende the hidden image when showPokemon props is true', async () => {});
